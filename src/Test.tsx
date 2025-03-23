195f8912ce0362078b71d9ce2ae538692a4d07a8
Anton Isakov <anton.isakov23@gmail.com>
2025-03-24 01:46:22 +0200

    setup multilanguage tool

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,10 +1,34 @@
 import Rating from "@mui/material/Rating";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+
+type Language = {
+  nativeName: string;
+};
+
+const lngs: Record<string, Language> = {
+  en: { nativeName: "English" },
+  lt: { nativeName: "Lietuviu" },
+};
 
 const Test = () => {
+  const { t } = useTranslation();
   return (
     <>
       <div>
-        test material UI <Rating />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              type="submit"
+              key={lng}
+              onClick={() => i18next.changeLanguage(lng)}
+              disabled={i18next.resolvedLanguage === lng}
+            >
+              {lngs[lng].nativeName}{" "}
+            </button>
+          ))}
+        </div>
+        {t("textfortesting")} <Rating />
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./i18next.js";
+import React from "react";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +13,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <React.Suspense fallback="loading">
+          <App />
+        </React.Suspense>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
