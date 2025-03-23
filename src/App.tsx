f00ec72b783ef128a2ecc93585575ea4165d7091
Anton Isakov <anton.isakov23@gmail.com>
2025-03-24 01:06:04 +0200

    Setup React Router and React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Test from "./Test";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +30,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
