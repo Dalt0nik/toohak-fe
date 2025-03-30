b729564cfe80e2b75ea1eeb878225e0f7ef96a27
saje9166 <sarunas.jemeljanovas@mif.stud.vu.lt>
2025-03-30 21:28:44 +0300

    Changed text to allow for translation

--- src/i18next.js
+++ src/i18next.js
@@ -2,7 +2,7 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
-import enTranslation from "./locale/en.json";
+import enTranslation from "../locales/en/translation.json";
 
 i18next
   .use(initReactI18next)
