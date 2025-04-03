c590da9ac4052dafa67d6772335fb8344cdfb19e
Tomas <tbdgacc@gmail.com>
2025-04-03 13:32:04 +0300

    JSF2025S-26: Added basic Error Boundary for 500 page + fixed minor merge mistake

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,41 @@ import "@assets/styles/App.css";
 import { Route, Routes } from "react-router-dom";
 import CreateQuizPage from "@pages/CreateQuizPage";
 import HomePage from "@pages/HomePage.tsx";
+import PageNotFound from "@pages/PageNotFound";
 import LobbyConnection from "@components/LobbyConnection";
 import Navbar from "@components/layout/Navbar.tsx";
-import QuizList from "./pages/QuizList";
+import QuizList from "@pages/QuizList";
 import { WebSocketProvider } from "@contexts/WebSocketContext";
 import { PrivateAppRoutes } from "@models/PrivateRoutes";
 import { PublicAppRoutes } from "@models/PublicRoutes";
+import ErrorBoundary from "@components/ErrorBoundary";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Navbar />}>
-        <Route path={PublicAppRoutes.HOME} element={<HomePage />} />
-        <Route
-          path={PrivateAppRoutes.LOBBY}
-          element={
-            <WebSocketProvider>
-              <LobbyConnection />
-            </WebSocketProvider>
-          }
-        />
-        <Route
-          path={PrivateAppRoutes.CREATE_QUIZ}
-          element={<CreateQuizPage />}
-        />
-        <Route path={PrivateAppRoutes.USER_QUIZZES} element={<QuizList />} />
-      </Route>
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route element={<Navbar />}>
+          <Route path={PublicAppRoutes.HOME} element={<HomePage />} />
+          <Route
+            path={PrivateAppRoutes.LOBBY}
+            element={
+              <WebSocketProvider>
+                <LobbyConnection />
+              </WebSocketProvider>
+            }
+          />
+          <Route
+            path={PrivateAppRoutes.CREATE_QUIZ}
+            element={<CreateQuizPage />}
+          />
+          <Route path={PrivateAppRoutes.USER_QUIZZES} element={<QuizList />} />
+          <Route
+            path={PublicAppRoutes.PAGE_NOT_FOUND}
+            element={<PageNotFound />}
+          />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
