68b3fd511974630637d2d287a036b758d404f9e7
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2025-04-07 13:27:20 +0300

    JSF2025S-44: Add calling /register on login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,8 @@
+import React, { useEffect } from "react";
 import useGetAccessToken from "@hooks/useGetAccessToken";
 import { AuthContext } from "@hooks/context/useAuthContext";
-import React from "react";
+import { registerUser } from "@api/userApi";
+import { Cookies } from "react-cookie";
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -8,11 +10,28 @@ interface AuthProviderProps {
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const { getAccessToken, isAuthenticated } = useGetAccessToken();
+  const cookies = new Cookies();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const checkForToken = () => {
+        const token = cookies.get("jwt");
+        if (token) {
+          registerUser().catch((err) => {
+            console.error("Registration error:", err);
+          });
+        } else {
+          console.warn("JWT token not available yet, retrying...");
+          setTimeout(checkForToken, 500);
+        }
+      };
+      checkForToken();
+    }
+  }, [isAuthenticated]);
 
   return (
-    <AuthContext.Provider
-      value={{ getAccessToken, isAuthenticated }}
-      children={children}
-    />
+    <AuthContext.Provider value={{ getAccessToken, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
